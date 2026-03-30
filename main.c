e39b1bd41d0db8a52e5c1802b0384cdc07c5eedb
GlebEvemi <gleb2466@gmail.com>
2026-03-30 15:16:03 +0300

    non important changes

--- main.c
+++ main.c
@@ -1,44 +1,77 @@
 #include "functions.h"
 
-commandFuncType getCommandHandlerByName(char*);
+commandFuncType getCommandHandlerByName(char *);
+
+int getInput(char *input);
 
 int main()
 {
-	//Initilize a default path as /home
-	chdir("/home");
-	while (1)
-	{
-		
-		char input[256];
-		myPath = getcwd(NULL, 0);
-		if (!myPath) {
-    		perror("getcwd failed");
-    		continue;
-		}
-		printf("->GL-Shell@%s: ", myPath);
-		fgets(input, sizeof(input), stdin);
-		char* commandName = strtok(input, " \n");
-		char* args = strtok(NULL, "\n");
-		if (!commandName){
-			free(myPath);
-			continue;
-		}
+    //Initilize a default path as /home
+    chdir("/home");
+    while (1) {
 
-		if (strcmp(commandName, END_CMD) == 0)
-		{
-			free(myPath);
-			break;
-		}
+        char input[256];
+        myPath = getcwd(NULL, 0);
+        assert(myPath != NULL && "getcwd failed");
+        printf("->GL-Shell@%s: ", myPath);
 
-        commandFuncType commandHandler = getCommandHandlerByName(commandName);
 
-        if(commandHandler == UNKNOWN_COMMAND){
-			free(myPath);
-			continue;
-		}
+        //Input handle
+        if (getInput(input)) {
+            perror("Input error");
+            free(myPath);
+            break;
+        }
+
+
+        char *commandName = strtok(input, " \n");
+        if (!commandName) {
+            free(myPath);
+            continue;
+        }
+
+
+        char *args = strtok(NULL, "\n");
+
+
+        if (strcmp(commandName, END_CMD) == 0) {
+            free(myPath);
+            break;
+        }
+
+        commandFuncType commandHandler =
+            getCommandHandlerByName(commandName);
+
+        if (commandHandler == UNKNOWN_COMMAND) {
+            free(myPath);
+            continue;
+        }
         commandHandler(args);
-		free(myPath);
-	}
 
-	return 0;
+        free(myPath);
+    }
+
+    return 0;
+}
+
+
+
+
+int getInput(char *input)
+{
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+        // EOF handle (Ctrl + D)
+        return 1;
+    }
+
+    size_t len = strlen(input);
+    if (len > 0 && input[len - 1] != '\n') {
+        // Input too long, clear the buffer
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+        printf
+            ("Ошибка: команда слишком длинная (максимум 255 символов)\n");
+        return 1;
+    }
+    return 0;
 }
